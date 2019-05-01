388da41e7b343a075de6f1caf701fe56b971b8e5
BiggerBrain <lishixiong.lsx@alipay.com>
2019-05-01 15:22:50 +0800

    抽象工厂模式开发

--- design-pattern/src/main/java/creational/patterns/factory/abs/IFactory.java
+++ design-pattern/src/main/java/creational/patterns/factory/abs/IFactory.java
@@ -7,10 +7,11 @@ package creational.patterns.factory.abs;
 /**
  * @author lishixiong
  * @version 1.0: IFactory.java, v 0.1 2019年04月30日 17:02 lishixiong Exp $
+ * 工厂接口
  */
 interface IFactory {
     /**
-     * 默认public，不需要写
+     * interface默认public，不需要写
      * @return
      */
      ISmsProduct createProduct1();
