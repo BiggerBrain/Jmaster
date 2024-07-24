6bfc2a9172d39bb95d6fb5205f3ddb028feba34c
lishixiong <standfordream@163.com>
2024-07-24 16:12:42 +0800

    构建二叉树

---
+++ java/src/main/java/Pattern.java
@@ -0,0 +1,27 @@
+import com.jayway.jsonpath.JsonPath;
+
+import java.util.List;
+
+public class Pattern {
+
+    public static void main(String[] args) {
+        String jsonPath = "[?(" +
+            "=310|=240".replaceAll(">=", "mt")
+                .replaceAll("<=", "le")
+                .replaceAll("\\|", "||")
+                .replaceAll("&", "&&")
+                .replaceAll(">", "\\$." + "code" + ">")
+                .replaceAll("<", "\\$." + "code" + "<")
+                .replaceAll("=", "\\$." + "code" + "==")
+                .replaceAll("mt", "\\$." + "code" + ">=")
+                .replaceAll("le", "\\$." + "code" + "<=")
+            + ")]";
+        System.out.println(jsonPath);
+        List<Object> matchers = JsonPath.read("{\"code\":310}", jsonPath);
+        System.out.println(!matchers.isEmpty());
+        matchers = JsonPath.read("{\"code\":240}", jsonPath);
+        System.out.println(!matchers.isEmpty());
+        matchers = JsonPath.read("{\"code\":11}", jsonPath);
+        System.out.println(!matchers.isEmpty());
+    }
+}
